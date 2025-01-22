433c5b8a1c6dcf6d21a6b63682da831224079822
ravindukd <ravindukd@gmail.com>
2025-01-22 11:57:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Snow IDE
+
+Snow IDE is a hobby project aimed at creating an IDE that allows developers to do coding with diagrams (UML).
+
+## Developed Using
+
+- **Electron**: Build cross-platform desktop apps with JavaScript, HTML, and CSS.
+- **React**: A JavaScript library for building user interfaces.
+- **React Router**: Declarative routing for React.
+- **Webpack**: A static module bundler for modern JavaScript applications.
+- **React Fast Refresh**: Fast Refresh is a React feature that allows you to get near-instant feedback for changes in your React components.
+
+## Work in Progress
+
+This project is currently under development. Features and functionalities are being actively worked on and improved.
+
+## License
+
+MIT © Ravindu Kavishka
---
+++ src/renderer/App.tsx
@@ -0,0 +1,50 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+function Hello() {
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200" alt="icon" src={icon} />
+      </div>
+      <h1>Snow IDE</h1>
+      <div className="Hello">
+        <a
+          href="https://Snow IDE.js.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              📚
+            </span>
+            Read our docs
+          </button>
+        </a>
+        <a
+          href="https://github.com/sponsors/Snow IDE"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="folded hands">
+              🙏
+            </span>
+            Donate
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
