548cf2c8b6ee042e808d99c43ae960d224709611
ravindukd <ravindukd@gmail.com>
2025-01-22 12:02:39 +0530

    Homepage testing

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -9,32 +9,6 @@ function Hello() {
         <img width="200" alt="icon" src={icon} />
       </div>
       <h1>Snow IDE</h1>
-      <div className="Hello">
-        <a
-          href="https://Snow IDE.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/Snow IDE"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
     </div>
   );
 }
